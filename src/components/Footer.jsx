e4b8a18a6b8c84b7011dde1330a122dbf7a6f656
fawad404 <fawadanxari31@gmail.com>
2025-03-05 09:32:13 +0500

    chagne name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -78,9 +78,9 @@ export const Footer = () => {
             <Link
               target="_blank"
               className="underline text-blue-400"
-              to="https://sujal-tangde.netlify.app/"
+              to="https://linkedin.com/in/fawadfiaz"
             >
-              Sujal Tangde
+              Fawad Fiaz
             </Link>
           </p>
           <p className="text-sm"> &copy;Copyright, All rights reserved.</p>
